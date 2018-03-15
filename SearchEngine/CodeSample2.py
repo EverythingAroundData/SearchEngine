91dfb80edb0a7bd95e35c0102da4d7aaad6f4d9f
EverythingAroundData <knowledgefreak23@gmail.com>
2018-03-15 21:56:19 +0530

    get next link

--- SearchEngine/CodeSample2.py
+++ SearchEngine/CodeSample2.py
@@ -1,3 +1,11 @@
 ﻿################################## Unit 2 ###########################################
 #Lesson 5
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">'
+'<div class="udacity float-left"><a href="http://twitter.com"><This is random text><a href="http://google.com">'
+)
 
+def get_next_link(page_content):
+    start_link = page.find('<a href=')
+
+print (url)
