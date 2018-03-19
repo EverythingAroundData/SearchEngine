caaf02bbc645c90ec4ecde58b5d1d4fe6ceb90d0
Mridul <mchandhok@ishir.com>
2018-03-19 14:32:47 +0530

    unit 3 file added

---
+++ SearchEngine/CodeSample3.py
@@ -0,0 +1,20 @@
+################################## Unit 3 ###########################################
+#Lesson 6
+#1. Median
+import math as m
+
+def median(a):
+    a.sort()
+    listlen = len(a)
+    idx = int(listlen/2)
+    if listlen % 2 == 1:
+        return (a[m.floor(idx)])
+    else:        
+        c = (a[idx]+a[idx-1])/2
+        return (c)
+
+a = [7, 8, 6, 5, 16]
+print (median(a))
+
+b = [5, 7, 12, 8]
+print (median(b))
