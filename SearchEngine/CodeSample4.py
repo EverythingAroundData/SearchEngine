bdab0ea1bef8ac03925b093ec90a47416d29aa6f
Mridul <mchandhok@ishir.com>
2018-03-26 20:20:39 +0530

    days between dates

--- SearchEngine/CodeSample4.py
+++ SearchEngine/CodeSample4.py
@@ -18,3 +18,44 @@ print_abacus(1337)
 print_abacus(4502)
 print ('My Server Ram')
 print_abacus(1024*16)
+
+
+def isleapyear(year):
+    if (year%4 == 0) and (year%400 == 0):
+        return 1
+    else:
+        return 0 
+
+def daysinmonth(month, year):
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    if month in (4, 6, 9, 11):
+        return 30
+    if month in (2):
+        a = isleapyear(year)
+        if a == 1:
+            return 29
+        else:
+            return 28
+    else:
+        return 'Invalid Month'
+
+def daysbetweendates(y1, m1, d1, y2, m2, d2):
+    if y1 > y2:
+        return 'Invalid operation'
+    else:
+        if m1 > m2:
+            return 'Invalid operation'
+        else:
+            if d1>d2:
+                return 'Invalid operation'
+            else:
+                if y1 == y2:
+                    if m1 == m2:
+                        return d2-d1
+                    else:
+                        for i in range[m1:m2+1]:
+
+
+                     
+   
