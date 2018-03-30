4fda98eb1dc35e105f31c7021be391625ec03172
Mridul <mchandhok@ishir.com>
2018-03-30 16:11:20 +0530

    Lists Practice file added

---
+++ SearchEngine/CodeSample5.py
@@ -0,0 +1,8 @@
+################### Miscellanious #########################
+
+# Name  Capital  Populations (millions)
+countries = [['China','Beijing',1350],
+             ['India','Delhi',1210],
+             ['Romania','Bucharest',21],
+             ['United States','Washington',307]]
+print (countries[1][1])
