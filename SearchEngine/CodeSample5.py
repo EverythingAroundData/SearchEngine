b39fc80f6cac06bb98e0419d9c4ff0c85a5c8cb8
Mridul <mchandhok@ishir.com>
2018-04-02 20:11:50 +0530

    new code added

--- SearchEngine/CodeSample5.py
+++ SearchEngine/CodeSample5.py
@@ -6,3 +6,28 @@ countries = [['China','Beijing',1350],
              ['Romania','Bucharest',21],
              ['United States','Washington',307]]
 print (countries[1][1])
+
+# Find no. of U in list 
+def measure_udacity(alist):
+    a=0
+    for i in alist:
+        for p in i:
+            if p=='U':
+                a=a+1
+    return a
+
+samplelist = ['Dave', 'Udacity', 'U2', 'AU', 'James', 'Au']
+
+print (measure_udacity(samplelist))
+
+# find index of search string in list
+
+def find_element(alist, fstring):
+    for i in alist:
+        if i==fstring:
+            return alist.index(fstring)
+    return -1
+
+fstr = 'AU2'
+    
+print (find_element(samplelist, fstr))
