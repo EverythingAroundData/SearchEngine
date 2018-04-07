ba47ce96f646e777e6ccc05e8dd4c8e6779cf363
EverythingAroundData <knowledgefreak23@gmail.com>
2018-04-07 20:22:57 +0530

    sudoku code added

--- SearchEngine/CodeSample6.py
+++ SearchEngine/CodeSample6.py
@@ -39,3 +39,89 @@ print (total_enrollment(usa_univs))
 print (total_enrollment(udacious_univs))
 
 ## Lesson 11, 2nd last program, and Lesson 12, 6th and 7th program  
+
+#sudoku
+# A valid sudoku square satisfies these
+# two properties:
+
+#   1. Each column of the square contains
+#       each of the whole numbers from 1 to n exactly once.
+
+#   2. Each row of the square contains each
+#       of the whole numbers from 1 to n exactly once.
+
+# You may assume the the input is square and contains at
+# least one row and column.
+
+s1 = [[1,2,3],
+      [2,3,1],
+      [3,1,2]]
+
+def check_sudoku(s):
+    a = 0
+    b = 0
+    c = 0
+    x = 0
+    y = 0
+    k = 0
+    listlen = []
+    newlist = []
+    for i in s:
+        a = a + 1
+        listlen.append(len(i))
+    for p in listlen:
+        if a!=p:
+            return
+    while b < a:
+        if b+1 not in list(range(a+1)[1::1]):
+            return 
+        b = b + 1
+    for e in s:
+        for f in e:
+            if f in list(map(chr, range(97,123))) or f in list(map(chr, range(65,91))) or f != int(f):
+                return False
+    c = sum(s[1])
+    while x < a:
+        while y < a:
+            k = k + s[y][x]
+            if y == a - 1:
+                newlist.append(k)
+            y = y + 1
+        k = 0
+        y = 0
+        x = x + 1
+    for m in newlist:
+        if m != c:
+            return False
+    return True
+
+s2 = [[1,2,3,4],
+      [2,3,1,3],
+      [3,1,2,3],
+      [4,4,4,4]]
+
+s3 = [[1,2,3,4],
+      [2,3,1,4],
+      [4,1,2,3],
+      [3,4,1,2]]
+
+s4 = [[1,2,3,4,5],
+      [2,3,1,5,6],
+      [4,5,2,1,3],
+      [3,4,5,2,1],
+      [5,6,4,3,2]]
+
+s5 = [['a','b','c'],
+      ['b','c','a'],
+      ['c','a','b']]
+
+s6 = [[1, 1.5],
+      [1.5, 1]]
+
+
+print (check_sudoku(s1)) 
+print (check_sudoku(s2)) 
+print (check_sudoku(s3)) 
+print (check_sudoku(s4)) 
+print (check_sudoku(s5)) 
+print (check_sudoku(s6)) 
