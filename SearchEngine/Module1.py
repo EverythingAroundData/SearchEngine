3321663b4204110c0fc3bec21b97f1790196e50b
Mridul <mchandhok@ishir.com>
2018-04-25 16:50:47 +0530

    Os mudule functions added

--- SearchEngine/Module1.py
+++ SearchEngine/Module1.py
@@ -23,4 +23,24 @@ import os
 oslist = dir(os)
 oslist.sort()
 
-print (oslist)
+#print (oslist)
+
+# 1. os.name
+
+print (os.name)
+
+# 2. os.getcwd
+
+print ('Current working Directory ' + os.getcwd())
+print ('Absolute Path ' + os.path.abspath('/'))
+dirlist = os.listdir('..')
+print ('list files and folders in cwd ' + str(dirlist))
+
+# 3. os.rename
+abspath = os.path.abspath('/') # returns E:\
+
+os.chdir(abspath)
+
+print (os.getcwd())
+
+os.rename('New.txt', 'Rename.txt')
