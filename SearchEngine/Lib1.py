c4217158b53b1bb1ce05d679870a5fad7116872f
Mridul <mchandhok@ishir.com>
2018-06-28 22:24:46 +0530

    usage of import of module/library

---
+++ SearchEngine/Lib1.py
@@ -0,0 +1,8 @@
+import urllib.request as r
+from Class1 import web as w
+
+MyFInstance = w()
+
+print (w.add_to_index([], 'linkedin', 'http://linkedin.com/'))
+
+#print (r.urlopen('http://linkedin.com/').read())
