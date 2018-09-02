9accc070b3018e4be49c116c8d475433d75662da
EverythingAroundData <knowledgefreak23@gmail.com>
2018-09-02 21:59:14 +0530

    retrieve json response from google api

---
+++ SearchEngine/Lib2_readAPI.py
@@ -0,0 +1,14 @@
+﻿import urllib.parse
+import requests
+
+
+
+main_api = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+address = '560036'
+
+url = main_api + urllib.parse.urlencode({'address':address}) 
+
+json_data = requests.get(url).json()
+
+print (json_data)
